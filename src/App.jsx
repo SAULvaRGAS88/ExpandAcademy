34d69da72ea6651319500dd842da99f74042cec0
Saul Vargas <saulvargascosta@hotmail.com>
2025-01-09 11:01:28 -0300

    Mui e login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BemVindo } from './paginas/bemVindo/BemVindo';
+import { Login } from './paginas/login/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BemVindo />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/paginas/bemVindo/BemVindo.jsx
+++ src/paginas/bemVindo/BemVindo.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 export const BemVindo = () => {
+
+  const handleClick = () => {
+    console.log('Clicou no botao')
+    window.location.href = '/login'
+  }
+
   return (
     <div className="container">
       <h1>Bem-vindo ao ExpandAcademy</h1>
-      <button className="btn btn-primary">Entrar</button>
+      <Button
+        variant="contained"
+        endIcon={<SendIcon />}
+        onClick={handleClick}>
+        Entrar
+      </Button>
     </div>
   )
 }
